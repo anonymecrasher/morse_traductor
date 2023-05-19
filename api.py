25695e7c69394d9dedc7228e4ecf7bc143a6372d
timtonix <timtonic@outlook.fr>
2023-05-19 14:59:43 +0200

    update the doc for api.py

--- api.py
+++ api.py
@@ -28,19 +28,10 @@ ALPHABET_TO_MORSE = {"A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", 
 
 def text_to_morse(value: str) -> str:
     """
+    Encode a text message into a morse one.
+
     :param value: (str type)
     :return morse_code: (str type)
-    Description FR:
-        -Cette fonction traduit un text "value" en morse grace a la bibliotech "ALPHABET_TO_MORSE".
-        (exemple SOS ->  ... --- ...)
-        -Cette fonction peut prendre en compte casiment toute les lettre de l'alphabet, les nombre, la ponctuation et
-        aussi la casi totalité des caractere spéciaux (meme si il est fortement déconseiller de les utilliser).
-    Description EN:
-        -This function translates a "value" text into morse thanks to the "ALPHABET_TO_MORSE" library.
-        (example SOS -> ... --- ...)
-        -This function can take into account all the letters of the alphabet, numbers, punctuation and
-        also all of the special characters (even if it is strongly advised not to use them).
-
     """
     value = value.upper()
     morse_code = ""
@@ -53,23 +44,10 @@ def text_to_morse(value: str) -> str:
 
 def morse_to_text(value: str) -> str:
     """
+    Decode morse into text message.
+
     :param value: (str type)
     :return text_code: (str  type)
-    Description FR:
-        -Cette fonction traduit le code mosre "value" en text grace a la bibliotech "MORSE_TO_ALPHABET".
-        (exemple ... --- ... ->  SOS)
-        -Cette fonction peut prendre en compte casiment toute les lettre de l'alphabet, les nombre, la ponctuation et
-        aussi la casi totalité des caractere spéciaux (meme si il est fortement déconseiller de les utilliser).
-        -ATTENTION nous uttilison le carractere "/" pour signifier un espace entre deux mots et nous utilison le
-        caractere " " pour signifier un changement de lettre.
-    Description EN:
-        -This function translates the mosre "value" code into text thanks to the "MORSE_TO_ALPHABET" library.
-        (example ... --- ... -> SOS)
-        -This function can take into account all the letters of the alphabet, numbers, punctuation and
-        also all of the special characters (even if it is strongly advised not to use them).
-        -ATTENTION we use the character "/" to mean a space between two words and we use the
-        character " " to signify a change of letter.
-
     """
     text_code = ""
     value = value.split(" ")
@@ -82,17 +60,9 @@ def morse_to_text(value: str) -> str:
 
 def split_morse(value: str) -> list:
     """
+    Encode a text message into a list of morse one.
     :param value: (str type)
     :return morse_code: (list type)
-    Description FR:
-        -Cette fonction appelle la fonction "text_to_morse" pour traduire un text "value" en morse et par la suite il va
-        séparer chaque lettre en uttilisant la fonction split pour renvoyer une liste contenant tout les caractères du
-        text "value".
-    Description EN:
-        -This function calls the "text_to_morse" function to translate a text "value" into morse and then it will
-        separate each letter using the split function to return a list containing all the characters of the
-        text "value".
-
     """
     morse_code = text_to_morse(value)
     return morse_code.split()
@@ -100,16 +70,10 @@ def split_morse(value: str) -> list:
 
 def convert_button_time(button_time: list) -> str:
     """
+    Convert the list of time into a symbol (morse)
+
     :param button_time: (list type)
     :return symbol: (str type)
-    Description FR:
-        -Cette fonction transforme une liste de temps en nanosecondes "button_time" sur bouton en code morse.
-        -ATTENTION les temps en entrés sont en nanoseconde et les valeur risque d'énormément changer au fil des mise a
-        jour.
-    Description EN:
-        -This function will convert a list of times from button_time to morse code.
-        -ATTENTION the input times are in nanoseconds and the values risk changing enormously over the updates.
-
     """
     symbol = ""
     for i in button_time:
