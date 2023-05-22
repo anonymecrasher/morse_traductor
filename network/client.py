362d5d5c03fd14dc31a59257486ee0a4f4a7a2e7
timtonix <timtonic@outlook.fr>
2023-05-22 08:20:58 +0200

    function that gen Ip addr Fix #43

--- network/client.py
+++ network/client.py
@@ -1,2 +1,16 @@
 import socket
 
+PORT = 2236
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("", PORT))
+print("socket actived")
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    if data.decode() == "ping":
+        print(f"pinged by {addr[0]}")
+        message = f"ping".encode()
+        sock.sendto(message, (addr[0], PORT))
+    else:
+        print(data)
